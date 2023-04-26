26566889c37c9d7a7a0b65dd08e4e18ef1a3ec7e
ChandraSekhar0711 <chandutogiri3@gmail.com>
2023-04-26 17:42:04 +0530

    changed height on mobile and tab devices

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -20,11 +20,12 @@ export class fetchQuery {
       }
     };
     try {
-      const res = await axios('https://openai80.p.rapidapi.com/chat/completions',options);
+      const res = await axios('https://openai80.p.rapidapi.com/chat/completions',options,setTimeout(5000));
       console.log(res.data.choices[0].message.content);
       return res.data.choices[0].message.content;
     } catch (error) {
       console.log(error.response.data.messages+". Please try by mentioning number of words you want like 150 or 100");
+      return error.response.data.messages+". Please try by mentioning number of words you want like 150 or 100";
     }
     
     //console.log(options.headers);
